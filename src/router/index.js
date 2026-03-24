b744349520675c573f40131707a5a8af61504ba7
evepoi <evepoi@naver.com>
2026-03-25 00:34:21 +0900

    빌드 배포 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,33 @@
+// src/router/index.js
 import { createRouter, createWebHashHistory } from "vue-router";
-
 import Home from "../views/Home.vue";
 import Ready from "../views/Ready.vue";
 import Exam from "../views/Exam.vue";
 import Result from "../views/Result.vue";
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory("/Bus_CBT/"),
   routes: [
-    { path: "/", component: Home },
-    { path: "/ready", component: Ready },
-    { path: "/exam", component: Exam },
-    { path: "/result", component: Result },
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/ready",
+      name: "ready",
+      component: Ready,
+    },
+    {
+      path: "/exam",
+      name: "exam",
+      component: Exam,
+    },
+    {
+      path: "/result",
+      name: "result",
+      component: Result,
+    },
   ],
 });
 
