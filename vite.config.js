2dbb6728fdfa4723f694ba032ab1c5537aa6417b
evepoi <evepoi@naver.com>
2026-03-24 23:53:52 +0900

    사이트 구성 완료 6차

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   base: "/Bus_CBT/",
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
   server: {
     host: true,
     port: 5173,
